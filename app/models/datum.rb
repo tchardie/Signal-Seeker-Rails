681ed70f4b97f58249ee8c8f3a9c49c78c7486d0
Tucker Hardie <tchardie@valdosta.edu>
2012-03-05 12:41:25 -0500

    added custom markers, fixed a few things

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -7,7 +7,17 @@ class Datum < ActiveRecord::Base
   def gmaps4rails_infowindow
     "<h4>Latitude: #{latitude}<br />Longitude: #{longitude}</h4>" << "<h4>#{carrier}<br />Wifi: #{wifi}<br />Cell: #{cell}</h4>"
   end
-  
+
+  def gmaps4rails_marker_picture
+    {
+      "picture" => "images/#{wifi}.png",
+      "width" =>  "32",          # string, mandatory
+      "height" => "32"     # string, mandatory
+    }
+  end
+
+  CARRIER_TYPES = ["Sprint", "At&t", "Verizon", "T-Mobile"]
+
   validates :wifi, :numericality => {:greater_than_or_equal_to => 0}
   validates :cell, :numericality => {:greater_than_or_equal_to => 0}
 end
