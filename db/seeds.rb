ff344beac654ba94d28e542ee91eeefcbd56e3d3
Andrew Parker <awparker@valdosta.edu>
2012-03-05 20:26:41 -0500

    Added some sample database seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,34 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-Datum.create(:location => '3.847425176936532, -85.28771829605103',
-:wifi => '1', :cell => '1', :carrier => 'Sprint',
-:gmaps => 'true', :latitude => '30.847425176936532', :longitude => '-85.28771829605103')
+Datum.create(:address => '30.845417,-83.289102', 
+:latitude => 30.845417, :longitude => -83.289102,
+:wifi => 1, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:location => '30.848539712297395, -88.28907012939453',
-:wifi => '2', :cell => '2', :carrier => 'Att',
-:gmaps => 'true', :latitude => '30.848539712297395', :longitude => '-88.28907012939453')
+Datum.create(:address => '30.846605,-83.290079', 
+:latitude => 30.846605, :longitude => -83.290079,
+:wifi => 3, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:location => '30.850271361383978, -91.28949928283691',
-:wifi => '3', :cell => '3', :carrier => 'Verizon',
-:gmaps => 'true', :latitude => '30.850271361383978', :longitude => '-91.28949928283691')
+Datum.create(:address => '30.847296,-83.289993', 
+:latitude => 30.847296, :longitude => -83.289993,
+:wifi => 2, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:location => '33.847425176936532, -83.28771829605103',
-:wifi => '4', :cell => '1', :carrier => 'Sprint',
-:gmaps => 'true', :latitude => '33.847425176936532', :longitude => '-83.28771829605103')
+Datum.create(:address => '30.846790,-83.288180', 
+:latitude => 30.846790, :longitude => -83.288180,
+:wifi => 4, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:location => '36.848539712297395, -83.28907012939453',
-:wifi => '0', :cell => '2', :carrier => 'Att',
-:gmaps => 'true', :latitude => '36.848539712297395', :longitude => '-83.28907012939453')
+Datum.create(:address => '30.847398,-83.287686', 
+:latitude => 30.847398, :longitude => -83.287686,
+:wifi => 0, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:location => '28.850271361383978, -83.28949928283691',
-:wifi => '1', :cell => '3', :carrier => 'Verizon',
-:gmaps => 'true', :latitude => '28.850271361383978', :longitude => '-83.28949928283691')
+Datum.create(:address => '30.848411,-83.289059', 
+:latitude => 30.848411, :longitude => -83.289059,
+:wifi => 3, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:location => '30.847425176936532, -76.28771829605103',
-:wifi => '1', :cell => '1', :carrier => 'Sprint',
-:gmaps => 'true', :latitude => '30.847425176936532', :longitude => '-76.28771829605103')
+Datum.create(:address => '30.849359,-83.289338', 
+:latitude => 30.849359, :longitude => -83.289338,
+:wifi => 1, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:location => '30.848539712297395, -45.28907012939453',
-:wifi => '3', :cell => '2', :carrier => 'Att',
-:gmaps => 'true', :latitude => '30.848539712297395', :longitude => '-45.28907012939453')
 
-Datum.create(:location => '52.850271361383978, -83.28949928283691',
-:wifi => '3', :cell => '3', :carrier => 'Verizon',
-:gmaps => 'true', :latitude => '52.850271361383978', :longitude => '-83.28949928283691')
 
-Datum.create(:location => '30.847425176936532, -71.28771829605103',
-:wifi => '2', :cell => '1', :carrier => 'Sprint',
-:gmaps => 'true', :latitude => '30.847425176936532', :longitude => '-71.28771829605103')
-
-Datum.create(:location => '14.848539712297395, -83.28907012939453',
-:wifi => '4', :cell => '2', :carrier => 'Att',
-:gmaps => 'true', :latitude => '14.848539712297395', :longitude => '-83.28907012939453')
-
-Datum.create(:location => '69.850271361383978, -83.28949928283691',
-:wifi => '0', :cell => '3', :carrier => 'Verizon',
-:gmaps => 'true', :latitude => '69.850271361383978', :longitude => '-83.28949928283691')
 
