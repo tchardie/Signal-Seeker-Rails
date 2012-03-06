e51e6a5b193c619771d0f76355c3e44b7583b45e
Tucker Hardie <tchardie@valdosta.edu>
2012-03-05 21:18:09 -0500

    Added location to data points

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,31 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-Datum.create(:address => '30.845417,-83.289102', 
+Datum.create(:location => 'Fine Arts', :address => '30.845417,-83.289102', 
 :latitude => 30.845417, :longitude => -83.289102,
 :wifi => 1, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:address => '30.846605,-83.290079', 
+Datum.create(:location => 'Pine Hall', :address => '30.846605,-83.290079', 
 :latitude => 30.846605, :longitude => -83.290079,
 :wifi => 3, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:address => '30.847296,-83.289993', 
+Datum.create(:location => 'The Loop', :address => '30.847296,-83.289993', 
 :latitude => 30.847296, :longitude => -83.289993,
 :wifi => 2, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:address => '30.846790,-83.288180', 
+Datum.create(:location => 'Lowndes Lobby', :address => '30.846790,-83.288180', 
 :latitude => 30.846790, :longitude => -83.288180,
 :wifi => 4, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:address => '30.847398,-83.287686', 
+Datum.create(:location => 'Patterson Lobby', :address => '30.847398,-83.287686', 
 :latitude => 30.847398, :longitude => -83.287686,
 :wifi => 0, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:address => '30.848411,-83.289059', 
+Datum.create(:location => 'NH2111', :address => '30.848411,-83.289059', 
 :latitude => 30.848411, :longitude => -83.289059,
 :wifi => 3, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
-Datum.create(:address => '30.849359,-83.289338', 
+Datum.create(:location => 'WH305', :address => '30.849359,-83.289338', 
 :latitude => 30.849359, :longitude => -83.289338,
 :wifi => 1, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
 
