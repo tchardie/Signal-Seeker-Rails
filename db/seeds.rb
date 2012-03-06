d8ef76cc2f0ae638814c861d33f80797c4dac223
Andrew Parker <awparker@valdosta.edu>
2012-03-05 22:02:59 -0500

    Modifications to the layout and database seeds, removed sign in button when on the sign in page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,31 +3,31 @@
 
 Datum.create(:location => 'Fine Arts', :address => '30.845417,-83.289102', 
 :latitude => 30.845417, :longitude => -83.289102,
-:wifi => 1, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
+:wifi => 1, :cell => 4, :carrier => 'Sprint',:gmaps => 'true')
 
 Datum.create(:location => 'Pine Hall', :address => '30.846605,-83.290079', 
 :latitude => 30.846605, :longitude => -83.290079,
-:wifi => 3, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
+:wifi => 3, :cell => 2, :carrier => 'Verizon',:gmaps => 'true')
 
 Datum.create(:location => 'The Loop', :address => '30.847296,-83.289993', 
 :latitude => 30.847296, :longitude => -83.289993,
-:wifi => 2, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
+:wifi => 2, :cell => 3, :carrier => 'At&t',:gmaps => 'true')
 
 Datum.create(:location => 'Lowndes Lobby', :address => '30.846790,-83.288180', 
 :latitude => 30.846790, :longitude => -83.288180,
-:wifi => 4, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
+:wifi => 4, :cell => 0, :carrier => 'T-Mobile',:gmaps => 'true')
 
 Datum.create(:location => 'Patterson Lobby', :address => '30.847398,-83.287686', 
 :latitude => 30.847398, :longitude => -83.287686,
-:wifi => 0, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
+:wifi => 0, :cell => 4, :carrier => 'Verizon',:gmaps => 'true')
 
 Datum.create(:location => 'NH2111', :address => '30.848411,-83.289059', 
 :latitude => 30.848411, :longitude => -83.289059,
-:wifi => 3, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
+:wifi => 3, :cell => 2, :carrier => 'Sprint',:gmaps => 'true')
 
 Datum.create(:location => 'WH305', :address => '30.849359,-83.289338', 
 :latitude => 30.849359, :longitude => -83.289338,
-:wifi => 1, :cell => 1, :carrier => 'Sprint',:gmaps => 'true')
+:wifi => 1, :cell => 3, :carrier => 'At&t',:gmaps => 'true')
 
 
 
