f73c483ff61f969bffc5cf5744df0466f8593ef1
Tucker Hardie <tucker.hardie@gmail.com>
2012-04-25 18:29:43 -0400

    edited data controller to work with update on android client.

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,5 +1,5 @@
 class DataController < ApplicationController
-  skip_before_filter :authorize, :only => [:index, :show, :new, :create, :destroy]
+  skip_before_filter :authorize, :only => [:index, :show, :new, :create, :destroy, :edit]
   # GET /data
   # GET /data.xml
   def index
